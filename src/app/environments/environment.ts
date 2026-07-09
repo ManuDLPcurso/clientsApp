eb51601b1065af2b0892d0363ef97089bb81095e
unknown <mdlpa.cursos@gmail.com>
2026-07-09 20:03:36 +0200

    eliminados

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = { 
   production:false,
-  supabaseUrl: 'https://icpfqcjnpecbbfpgjjkj.supabase.co',
-  supabaseKey: 'sb_publishable_GMGgMdGzhHwXCkbx-B4auA_Fy5WoHEG'
+  supabaseUrl: '',
+  supabaseKey: ''
 
 };
